b205f0dc925954c3443fddf3d84c4cd4dbe47133
Nandita D <bubble8773@users.noreply.github.com>
2021-12-17 20:28:47 +0530

    Form start to Wood1

---
+++ Code/0ToWood1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+ float distanceSqrt(int x1, int y1, int x2, int y2)
+{
+    // Calculating distance
+    return sqrt(pow(x2 - x1, 2) +
+                pow(y2 - y1, 2) * 1.0);
+}
+
+int main()
+{
+
+    // game loop
+    while (1) {
+        int x;
+        int y;
+        int next_checkpoint_x; // x position of the next check point
+        int next_checkpoint_y; // y position of the next check point
+        int next_checkpoint_dist; // distance to the next checkpoint
+        int next_checkpoint_angle; // angle between your pod orientation and the direction of the next checkpoint
+        cin >> x >> y >> next_checkpoint_x >> next_checkpoint_y >> next_checkpoint_dist >> next_checkpoint_angle; cin.ignore();
+       
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+
+        // You have to output the target position
+        // i.e.: "x y thrust"
+
+         cout << next_checkpoint_x << " " << next_checkpoint_y << " " <<"80"<< endl;
+    }
+}
+
